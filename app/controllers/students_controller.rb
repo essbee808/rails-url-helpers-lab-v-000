4bfbe440078c8af4ba325cd222c816b9c3a1730b
Sheena Bonilla <sheena.myb@gmail.com>
2019-09-21 06:36:37 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,11 +10,11 @@ class StudentsController < ApplicationController
 
   def activate_student
     student = Student.find(params[:id])
-    if student.active == "false"
-      student.active == "true"
+    if student.active == false
+      student.active == true
       student.save
-    elsif student.active == "true"
-      student.active == "false"
+    elsif student.active == true
+      student.active == false
       student.save
     end
     redirect_to student_path(student)
