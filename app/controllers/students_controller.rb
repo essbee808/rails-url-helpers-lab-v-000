060cd6bc0bf1aad77aff39ff1a4cb2ee690060d8
Sheena Bonilla <sheena.myb@gmail.com>
2019-09-21 05:23:59 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,8 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
     if @student.active == "false"
+    else
+    end
   end
 
   # def activate
