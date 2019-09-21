5237d98c17f58ae20fc527a8706a449511fd3da1
Sheena Bonilla <sheena.myb@gmail.com>
2019-09-21 05:19:10 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,10 +3,10 @@ class Student < ActiveRecord::Base
     self.first_name + " " + self.last_name
   end
 
-  # def activate_student
+  def activate_student
   #   if self.active == "false"
   #     self.active = "true"
   #   else
   #     self.active = "false"
-  # end
+  end
 end
